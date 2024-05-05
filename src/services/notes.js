55826b9d8b909ac58c915acfc7d88b8618f749fc
Diegordo17 <diegosolorzano608@gmail.com>
2024-05-05 14:55:24 -0600

    Change url

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://part3h.onrender.com'
 //All the methods
 const getAll = () => {
   const request = axios.get(baseUrl)
